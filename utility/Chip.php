8ef44c9ab1ad86be635161d9e7dfc9084a9bedac
baturkacamak <batisonline@gmail.com>
2022-12-07 11:33:24 +0100

    added code comments

--- utility/Chip.php
+++ utility/Chip.php
@@ -2,6 +2,7 @@
 
 namespace Utility;
 
+// Check if the 'Utility\Chip' class already exists
 if (! class_exists('Utility\Chip')) {
     /**
      * Class Chip
@@ -10,6 +11,8 @@ if (! class_exists('Utility\Chip')) {
     {
         /**
          * The one true instance.
+         *
+         * @var Chip
          */
         private static $instance;
 
@@ -18,43 +21,67 @@ if (! class_exists('Utility\Chip')) {
          */
         protected function __construct()
         {
+            // Set the instance property to the current instance
             return self::$instance = $this;
         }
 
         /**
          * Get singleton instance.
          *
+         * @return Chip
+         *
          * @since 1.5
          */
         public static function getInstance()
         {
+            // Check if the instance property is not set
             if (! isset(self::$instance)) {
+                // Create a new instance of the class
                 self::$instance = new self();
             }
 
+            // Return the instance property
             return self::$instance;
         }
 
         /**
          * Initializer
+         *
          * @throws \Exception
          */
         public function init()
         {
+            // Set the base URL for the files we want to download
             $baseUrl = 'https://www.chip.com.tr/images/pdf/b/';
-            for ($year = 1997; $year <= 2018; $year++) {
-                for ($month = 1; $month <= 12; $month++) {
-                    for ($page = 1; $page <= 300; $page++) {
+
+            // Loop through all years from 1997 to 2018
+            for ($year = 1997; $year <= 2018;) {
+                // Loop through all months from 1 to 12
+                for ($month = 1; $month <= 12;) {
+                    // Loop through all pages from 1 to 300
+                    for ($page = 1; $page <= 300;) {
+                        // Create the full URL of the file we want to download
                         $fileUrl = $baseUrl . "{$year}-{$month}-{$page}.jpg";
+
+                        // Try to download the file
                         try {
+                            // Create a new instance of the 'Utility\Downloader' class,
+                            // passing the URL of the file we want to download
                             new \Utility\Downloader($fileUrl);
                         } catch (\Exception $e) {
+                            // If an error occurs, break out of the innermost loop
                             break 1;
                         }
+                        // Increment the page number
+                        $page++;
                     }
+                    // Increment the month number
+                    $month++;
                 }
+                // Increment the year number
+                $year++;
             }
-
         }
     }
 }
+
--- utility/Downloader.php
+++ utility/Downloader.php
@@ -2,10 +2,12 @@
 
 namespace Utility;
 
+// Import the 'GuzzleHttp\Client' and 'GuzzleHttp\Exception\ClientException' and 'GuzzleHttp\Exception\GuzzleException' classes
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 
+// Check if the 'Utility\Downloader' class already exists
 if (! class_exists('Utility\Downloader')) {
     /**
      * Class Downloader
@@ -14,10 +16,14 @@ if (! class_exists('Utility\Downloader')) {
     class Downloader
     {
         /**
-         * @var
+         * The URL of the file to download.
+         *
+         * @var string
          */
         private $fileUrl;
         /**
+         * The Guzzle HTTP client instance.
+         *
          * @var Client
          */
         private $guzzle;
@@ -25,14 +31,18 @@ if (! class_exists('Utility\Downloader')) {
         /**
          * Downloader constructor.
          *
-         * @param $fileUrl
+         * @param string $fileUrl The URL of the file to download.
          *
-         * @throws \Exception
+         * @throws \Exception If the file cannot be downloaded.
          */
         public function __construct($fileUrl)
         {
+            // Set the file URL property to the value of the '$fileUrl' parameter
             $this->fileUrl = $fileUrl;
-            $this->guzzle  = new Client(
+
+            // Create a new instance of the 'GuzzleHttp\Client' class,
+            // and set the 'User-Agent' header to a specific value
+            $this->guzzle = new Client(
                 [
                     'headers' => [
                         'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36' . ' (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
@@ -40,54 +50,83 @@ if (! class_exists('Utility\Downloader')) {
                 ]
             );
 
+            // Call the 'download()' method to start the download
             $this->download();
         }
 
+        /**
+         * Check if the file exists on the remote server.
+         *
+         * @return bool
+         */
         private function checkRemoteFile()
         {
+            // Try to get the HTTP headers of the file at the specified URL
             try {
                 $this->guzzle->head($this->fileUrl);
 
+                // If the file exists, return true
                 return true;
             } catch (ClientException $client_exception) {
+                // If the file does not exist, return false
                 return false;
             }
         }
 
         /**
-         * @throws \Exception
+         * Download the file from the remote server.
+         *
+         * @throws \Exception If the file cannot be downloaded.
          */
         private function download()
         {
+            // Check if the file does not exist locally and exists on the remote server
             if (! file_exists($localFile = $this->getFileDirectory()) && $this->checkRemoteFile()) {
+                // Try to download the file
                 try {
+                    // Use the Guzzle client to send a GET request to the file URL,
+                    // and save the file to the local file path
                     $this->guzzle->request(
                         'GET',
                         $this->fileUrl,
                         ['sink' => $localFile]
                     );
                 } catch (GuzzleException $e) {
+                    // If an error occurs while downloading the file,
+                    // do nothing (just catch the exception and continue)
                 }
             } elseif (! file_exists($localFile) && ! $this->checkRemoteFile()) {
+                // If the file does not exist locally and does not exist on the remote server,
+                // throw an exception
                 throw new \Exception('All files exists');
             }
         }
 
+        /**
+         * Get the local file path for the file.
+         *
+         * @return string
+         */
         private function getFileDirectory()
         {
+            // Parse the file URL to get the file name and the directory it is in
             $parsed_url = parse_url($this->fileUrl);
             $file_name  = basename($parsed_url['path']);
             $exploded   = explode('-', $file_name);
             $year       = $exploded[0];
             $month      = $exploded[1];
 
+            // Build the local file path based on the file name and the directory it is in
             $directory = BASE_DIR . '/static/magazins' . "/{$exploded[0]}/$exploded[1]";
             if (! is_dir($directory)) {
-                //Create our directory.
+                // If the directory does not exist, create it
                 mkdir($directory, 0755, 1);
             }
 
+            // Return the local file path
             return $directory . "/{$exploded[2]}";
         }
     }
 }
+
+

85c7929b12a0db55b67cff1bd4ce3dcf33f001f6
baturkacamak <batisonline@gmail.com>
2022-12-07 16:09:12 +0100

    added comments

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 
-// silence
+// Import the 'Chip' class from the 'Utility' namespace
 use Utility\Chip;
 
+// Enable error reporting and display of errors
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Include the Composer autoloader
 include_once 'vendor/autoload.php';
+
+// Define the BASE_DIR constant
 defined('BASE_DIR') or define('BASE_DIR', __DIR__);
+
 try {
+    // Initialize the Chip class
     Chip::getInstance()->init();
 } catch (Exception $e) {
+    // Do nothing if an exception is thrown
 }

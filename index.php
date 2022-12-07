780f2183089bc63fd9161b9b0b8e19ab65d2b300
baturkacamak <batisonline@gmail.com>
2022-12-07 14:59:52 +0100

    added try catch

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
 // silence
+use Utility\Chip;
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 include_once 'vendor/autoload.php';
 defined('BASE_DIR') or define('BASE_DIR', __DIR__);
-\Utility\Chip::getInstance()->init();
+try {
+    Chip::getInstance()->init();
+} catch (Exception $e) {
+}
